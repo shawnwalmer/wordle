2653d1ec1eccc07eb75bc8b5bd76d1be2b706972
shawnwalmer <43099753+shawnwalmer@users.noreply.github.com>
2022-02-02 10:27:23 -0600

    Literal check working. -scw

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import sys
+
 # The first round you manually select "AROSE"
-game = ["A", 0, 0, 0, 0]
+literal = ["A", 0, "G", "I", 0]
+contains = []
+
+print("Kicking off script...")
+print("Checking for literal matches for " + str(literal))
 
 # If there are no matches select "UNTIL"
-if game == [0, 0, 0, 0, 0]:
+if literal == [0, 0, 0, 0, 0]:
     print("UNTIL")
     sys.exit()
 
+# Initializing valid. This variable will hold all valid answers for literal assignment (full match)
+valid = set()
+
 with open("fives.txt", "r") as fives:
     for word in fives:
+        valid.add(word)
         x = range(5)
         for y in x:
-            if game[y] != 0:
-                if word[y] != game[y]:
+            if literal[y] != 0:
+                if word[y] != literal[y]:
+                    if word in valid:
+                        valid.remove(word)
+
+if len(valid) == 0:
+    print("Wow, such empty.")
+    sys.exit()
+else:
+    print(str(len(valid)) + " valid after literal check.")
+
