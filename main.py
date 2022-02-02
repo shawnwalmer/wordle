f357e27de56873f9663f486efc0e9f96298c9665
shawnwalmer <43099753+shawnwalmer@users.noreply.github.com>
2022-02-01 18:26:28 -0600

    Static analysis done. Starting code. -scw

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import csv
-holding = []
-with open('fives.txt', 'r') as fd:
-    reader = csv.reader(fd)
-    for row in reader:
-        if len(row[0]) == 5:
-            print(len(row[0]))
+import sys
+# The first round you manually select "AROSE"
+game = ["A", 0, 0, 0, 0]
+
+# If there are no matches select "UNTIL"
+if game == [0, 0, 0, 0, 0]:
+    print("UNTIL")
+    sys.exit()
+
+with open("fives.txt", "r") as fives:
+    for word in fives:
+        x = range(5)
+        for y in x:
+            if game[y] != 0:
+                if word[y] != game[y]:
